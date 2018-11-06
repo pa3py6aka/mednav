f5b4d68d3358f7863dd2f4b4628afc0a9d997cbc
Pa3Py6aka <alexgref@ya.ru>
2018-11-06 22:21:21 +0300

    fix

--- frontend/views/company/company/_menu.php
+++ frontend/views/company/company/_menu.php
@@ -8,7 +8,7 @@
     <li><a href="<?= $company->getUrl('contacts') ?>">Контакты</a></li>
     <li><a href="<?= $company->getUrl('trades') ?>">Товары</a> <sup><?= $company->getCountFor('trades') ?></sup></li>
     <li><a href="<?= $company->getUrl('boards') ?>">Объявления</a> <sup><?= $company->getCountFor('boards') ?></sup></li>
-    <li><a href="<?= $company->getUrl('сnews') ?>">Новости компании</a> <sup><?= $company->getCountFor('сnews') ?></sup></li>
+    <li><a href="<?= $company->getUrl('cnews') ?>">Новости компании</a> <sup><?= $company->getCountFor('сnews') ?></sup></li>
     <li><a href="<?= $company->getUrl('articles') ?>">Статьи</a> <sup><?= $company->getCountFor('articles') ?></sup></li>
     <li><a href="#">Акции</a> <sup>1200</sup></li>
 </ul>

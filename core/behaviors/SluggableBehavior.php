2c35176c51f86e818b1e0829084587ac8d7cc0b6
Pa3Py6aka <alexgref@ya.ru>
2018-11-15 21:46:54 +0300

    Update sluggable behavior: transliterate update

--- core/behaviors/SluggableBehavior.php
+++ core/behaviors/SluggableBehavior.php
@@ -4,6 +4,7 @@ namespace core\behaviors;
 
 
 use yii\helpers\ArrayHelper;
+use yii\helpers\Inflector;
 use Zelenin\yii\behaviors\Slug;
 
 class SluggableBehavior extends Slug
@@ -29,7 +30,7 @@ class SluggableBehavior extends Slug
                 }
                 $slugParts[] = $part;
             }
-            $slug = substr($this->generateSlug($slugParts), 0, $this->slugLength);
+            $slug = $this->generateSlug($slugParts);
             if (substr($slug, -1) == '-') {
                 $slug = substr($slug, 0, -1);
             }
@@ -38,6 +39,63 @@ class SluggableBehavior extends Slug
         }
 
         return $this->ensureUnique ? $this->makeUnique($slug) : $slug;
+    }
+
+    protected function generateSlug($slugParts): string
+    {
+        $slug = $this->transliterate(implode('-', $slugParts));
+        return substr(Inflector::slug($slug), 0, $this->slugLength);
+    }
+
+    protected function transliterate($str): string
+    {
+        // ГОСТ 7.79B
+        $transliteration = array(
+            'А' => 'A', 'а' => 'a',
+            'Б' => 'B', 'б' => 'b',
+            'В' => 'V', 'в' => 'v',
+            'Г' => 'G', 'г' => 'g',
+            'Д' => 'D', 'д' => 'd',
+            'Е' => 'E', 'е' => 'e',
+            'Ё' => 'Yo', 'ё' => 'yo',
+            'Ж' => 'Zh', 'ж' => 'zh',
+            'З' => 'Z', 'з' => 'z',
+            'И' => 'I', 'и' => 'i',
+            'Й' => 'J', 'й' => 'j',
+            'К' => 'K', 'к' => 'k',
+            'Л' => 'L', 'л' => 'l',
+            'М' => 'M', 'м' => 'm',
+            'Н' => "N", 'н' => 'n',
+            'О' => 'O', 'о' => 'o',
+            'П' => 'P', 'п' => 'p',
+            'Р' => 'R', 'р' => 'r',
+            'С' => 'S', 'с' => 's',
+            'Т' => 'T', 'т' => 't',
+            'У' => 'U', 'у' => 'u',
+            'Ф' => 'F', 'ф' => 'f',
+            'Х' => 'H', 'х' => 'h',
+            'Ц' => 'Cz', 'ц' => 'cz',
+            'Ч' => 'Ch', 'ч' => 'ch',
+            'Ш' => 'Sh', 'ш' => 'sh',
+            'Щ' => 'Shh', 'щ' => 'shh',
+            'Ъ' => 'ʺ', 'ъ' => 'ʺ',
+            'Ы' => 'Y`', 'ы' => 'y`',
+            'Ь' => '', 'ь' => '',
+            'Э' => 'E`', 'э' => 'e`',
+            'Ю' => 'Yu', 'ю' => 'yu',
+            'Я' => 'Ya', 'я' => 'ya',
+            '№' => '#', 'Ӏ' => '‡',
+            '’' => '`', 'ˮ' => '¨',
+        );
+
+        $str = strtr($str, $transliteration);
+        $str = mb_strtolower($str, 'UTF-8');
+        $str = preg_replace('|([\s]+)|s', '-', $str);
+        $str = preg_replace('/[^0-9a-z\-]/', '', $str);
+        $str = preg_replace('|([-]+)|s', '-', $str);
+        $str = trim($str, '-');
+
+        return $str;
     }
 
     /*protected function generateSlug($slugParts)

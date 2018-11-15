2eb84ca3b23102d45e6d2f247acdf5614a03e592
Pa3Py6aka <alexgref@ya.ru>
2018-11-15 19:57:30 +0300

    Update

--- frontend/views/user/_left_menu.php
+++ frontend/views/user/_left_menu.php
@@ -19,15 +19,21 @@ $link = Yii::$app->controller->action->id;
     <div class="panel-body">
         <ul class="nav nav-pills nav-stacked">
             <li role="presentation"<?= $link == 'profile' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/account/profile']) ?>">Мои данные</a></li>
+            <li role="presentation"<?= $controller == 'user/message' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/message/dialogs']) ?>">Сообщения<?= DialogHelper::getNewMessagesCount($user) ?></a></li>
+
             <?php if ($user->isCompany()): ?>
                 <li role="presentation"<?= $link == 'company' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/account/company']) ?>"><?= Yii::$app->settings->get(Settings::COMPANY_NAME_UP) ?></a></li>
+            <?php endif; ?>
+
+            <li role="presentation"<?= $controller == 'user/board' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/board/active']) ?>"><?= Yii::$app->settings->get(Settings::BOARD_NAME_UP) ?></a></li>
+
+            <?php if ($user->isCompany()): ?>
                 <li role="presentation"<?= $controller == 'user/trade' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/trade/active']) ?>"><?= Yii::$app->settings->get(Settings::TRADE_NAME_UP) ?></a></li>
                 <li role="presentation"<?= $controller == 'user/cnews' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/cnews/active']) ?>"><?= Yii::$app->settings->get(Settings::CNEWS_NAME_UP) ?></a></li>
-                <li role="presentation"<?= $controller == 'user/expo' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/expo/active']) ?>"><?= Yii::$app->settings->get(Settings::EXPO_NAME_UP) ?></a></li>
+                <?php /*<li role="presentation"<?= $controller == 'user/expo' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/expo/active']) ?>"><?= Yii::$app->settings->get(Settings::EXPO_NAME_UP) ?></a></li> */ ?>
             <?php endif; ?>
+
             <li role="presentation"<?= $controller == 'user/order' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/order/orders']) ?>">Заказы<?= OrderHelper::getNewOrdersCount($user) ?></a></li>
-            <li role="presentation"<?= $controller == 'user/message' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/message/dialogs']) ?>">Сообщения<?= DialogHelper::getNewMessagesCount($user) ?></a></li>
-            <li role="presentation"<?= $controller == 'user/board' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/board/active']) ?>"><?= Yii::$app->settings->get(Settings::BOARD_NAME_UP) ?></a></li>
             <li role="presentation"<?= $controller == 'user/support' ? ' class="active"' : '' ?>><a href="<?= Url::to(['/user/support/dialogs']) ?>">Служба поддержки<?= DialogHelper::getSupportNewMessagesCount($user) ?></a></li>
         </ul>
     </div>

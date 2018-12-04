c2363676f5a8de0b605e9bb03f76769944c6c841
Pa3Py6aka <alexgref@ya.ru>
2018-12-04 16:37:50 +0300

    Update

--- frontend/views/company/company/_menu.php
+++ frontend/views/company/company/_menu.php
@@ -8,6 +8,6 @@
     <li><a href="<?= $company->getUrl('contacts') ?>">Контакты</a></li>
     <li><a href="<?= $company->getUrl('trades') ?>">Товары</a> <sup><?= $company->getCountFor('trades') ?></sup></li>
     <li><a href="<?= $company->getUrl('boards') ?>">Объявления</a> <sup><?= $company->getCountFor('boards') ?></sup></li>
-    <li><a href="<?= $company->getUrl('cnews') ?>">Новости компании</a> <sup><?= $company->getCountFor('сnews') ?></sup></li>
+    <li><a href="<?= $company->getUrl('cnews') ?>">Новости компании</a> <sup><?= $company->getCountFor('cnews') ?></sup></li>
     <li><a href="<?= $company->getUrl('articles') ?>">Статьи</a> <sup><?= $company->getCountFor('articles') ?></sup></li>
 </ul>

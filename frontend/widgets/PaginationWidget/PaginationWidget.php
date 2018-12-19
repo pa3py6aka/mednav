5eb188dddf3789f2abff403e2d626a60eb9cb9f0
Pa3Py6aka <alexgref@ya.ru>
2018-12-19 22:14:35 +0300

    Update

--- frontend/widgets/PaginationWidget/PaginationWidget.php
+++ frontend/widgets/PaginationWidget/PaginationWidget.php
@@ -19,15 +19,15 @@ class PaginationWidget extends Widget
 
     public function run()
     {
-        $type = PaginationHelper::PAGINATION_NUMERIC;
+        if ($this->provider->pagination->pageCount <= $this->provider->pagination->page + 1) {
+            return '';
+        }
+
+        $type = PaginationHelper::PAGINATION_SCROLL;
         if ($this->geo) {
             $type = PaginationHelper::PAGINATION_SCROLL;
         } else if ($this->category && $this->category->pagination == PaginationHelper::PAGINATION_NUMERIC) {
             $type = PaginationHelper::PAGINATION_NUMERIC;
-        } else if ($this->provider->pagination->pageCount > $this->provider->pagination->page + 1) {
-            $type = PaginationHelper::PAGINATION_SCROLL;
-        } else {
-            return '';
         }
 
         return $this->render('block', [

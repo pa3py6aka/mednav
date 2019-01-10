e290cf0b426b92b40f4f2f423fdb6c33448e148a
Pa3Py6aka <alexgref@ya.ru>
2019-01-10 18:36:38 +0300

    Update

--- common/mail/auth/reset-password.php
+++ common/mail/auth/reset-password.php
@@ -4,8 +4,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $user \core\entities\User\User */
 
-$this->title = 'Сброс пароля';
+$this->title = 'Восстановление доступа';
 $confirmLink = Yii::$app->urlManager->createAbsoluteUrl(['auth/reset/confirm', 'token' => $user->password_reset_token]);
 ?>
-<p>Перейдите по этой ссылке для броса пароля:</p>
+<p>Перейдите по этой ссылке для сброса пароля:</p>
 <p><?= Html::a(Html::encode($confirmLink), $confirmLink) ?></p>
